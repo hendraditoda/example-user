318693f17e56ab6d6e8d570bdef6b9fb46cb99dc
Hendradito Dwi A <hendradito21@gmail.com>
2022-09-26 21:35:47 +0700

    Update Layout.js

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -21,9 +21,9 @@ const LayoutComponent = (props) => {
         <Layout>
           <Layout style={{ padding: '0 24px 24px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
               <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item> */}
             </Breadcrumb>
             <Content className="site-layout-background">
               {props.content}
